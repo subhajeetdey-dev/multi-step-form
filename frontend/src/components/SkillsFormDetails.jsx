c167a453e5bdddc2ec3cc1cb8ad02370785643f0
Subha1012pixel <subhajeetdeywork@gmail.com>
2026-01-22 22:08:28 +0530

    Setup Skill Form

--- frontend/src/components/SkillsFormDetails.jsx
+++ frontend/src/components/SkillsFormDetails.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+"use client";
 
-const SkillsFormDetails = () => {
+export default function SkillsFormDetails ({formData, handleChange}){
+
   return (
-    <div>
-      hey
-    </div>
-  )
-}
-
-export default SkillsFormDetails
+      <div className="border rounded-lg p-8">
+        <div className="flex flex-col gap-5">
+          <div className="grid grid-cols-2 items-center">
+            <span className="font-bold">Skills</span>
+            <input name="skills" value={formData.skills} onChange={handleChange} required
+              className="w-80 rounded-sm border"
+              placeholder="Enter your skills"
+            />
+          </div>
+          <div className="grid grid-cols-2 items-center">
+            <span className="font-bold">Experience</span>
+            <input name="experience" value={formData.experience} onChange={handleChange} required
+              className="w-80 rounded-sm border"
+              placeholder="Enter your experience"
+            />
+          </div>
+        </div>
+      </div>
+  );
+};
